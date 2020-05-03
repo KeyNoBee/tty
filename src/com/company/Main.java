030b68d0ebd3050145baeb63620547ce9e4e42af
igor <apxahre1@ukr.net>
2020-05-03 10:25:24 +0300

    ddd

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int a = 99;
+        int b = 3;
+        int c = a % b;
+
+            System.out.println(c);
+            if (c==0) {
+                System.out.println("True");
+            }
+            else   {
+            System.out.println("False");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
